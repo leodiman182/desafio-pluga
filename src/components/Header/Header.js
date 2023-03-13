075ffccb1eb63a0853c9221d932a6157909125ca
Leonardo <89220518+leodiman182@users.noreply.github.com>
2023-03-13 08:32:55 -0300

    Header ok

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './style.css';
-
 import plugaLogo from '../../assets/pluga-logo.png';
+
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Header = () => {
   return (
     <section className="header-bar">
       <div className='bar-div'>
-        <div className='sec1'>
+        {/* <div className='sec1'> */}
           <img className='logo' src={ plugaLogo } alt="" />
-        </div>
-        <div className='sec1'>
-          <input type="text" />
+        {/* </div> */}
+        <div className='sec2-wrapper'>
+          <AiOutlineSearch className='search-icon' />
+          <input className='font1 search-input' placeholder='Buscar +80 ferramentas' type="text" />
         </div>
       </div>
     </section>
