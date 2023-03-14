4a2d0ecb9d7d3293c499f22b0f1a4d7a7e6ea5ae
Leonardo <89220518+leodiman182@users.noreply.github.com>
2023-03-14 17:45:05 -0300

    Ajustes layout home + começo do filtro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,66 @@
 import React, { useContext, useEffect, useState } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import MainContext from '../../context/MainContext';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import ToolCard from '../../components/ToolCard';
 import Modal from '../../components/Modal';
 
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineLoading } from "react-icons/ai";
 
 import './style.css';
 
-import { apiMock } from '../../mocks/api12';
-
 const Home = () => {
   const {
-    // api, setApi, data, setData,
+    api, setApi, data, setData,
     setModalOpen, modalOpen,
-    setSelectedTool, previouslySelected, setPreviouslySelected } = useContext(MainContext);
+    setSelectedTool,
+    previouslySelected, setPreviouslySelected
+  } = useContext(MainContext);
+
   const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const URL = 'https://pluga.co/ferramentas_search.json';
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(URL)
+      .then(res => {
+        const array = res.data;
+
+        const newArray = array.sort((a, b) => {
+          let fa = a.name.toLowerCase(),
+              fb = b.name.toLowerCase();
+      
+          if (fa < fb) {
+              return -1;
+          }
+          if (fa > fb) {
+              return 1;
+          }
+
+          return 0;
+        });
+
+        setApi(newArray);
+        setData(newArray);
+        setLoading(false);
+      })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    const newList = inputValue !== ''
+    ? data.filter(query => query.name.toLowerCase().match(inputValue.toLowerCase()))
+    : setApi(data);
+
+    setApi(newList)
+
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValue])
 
   function handleToolSelect (el) {    
     const array = previouslySelected;
@@ -58,39 +101,6 @@ const Home = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [modalOpen])
 
-  // useEffect(() => {
-  //   function handleQuery () {
-  //     const newList = inputValue === ''
-  //     ? apiMock
-  //     : apiMock.filter((query) => {
-  //         return query.name.toLowerCase().includes(query.toLowerCase())
-  //       });
-
-  //     setData(newList);
-  //   }
-
-  //   handleQuery();
-  //   console.log(inputValue);
-
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [inputValue])
-
-  // useContext(() => {
-  //   console.log('oi');
-  //   axios
-  //     .get('https://pluga.co/ferramentas_search.json')
-  //     .then(res => {
-  //       console.log(res.data);
-  //       setApi(res.data)
-  //       setData(res.data)
-  //     })
-  // }, []);
-  
-  // useContext(() => {
-  //   console.log(api);
-  //   console.log(data);
-  // }, [api, data]);
-
   return (
     <main className="app">
       {
@@ -114,34 +124,49 @@ const Home = () => {
               type="text"
             />
           </div>
-          <button className='button'>
+          <button onClick={() => setApi(data)} className='button'>
             EXIBIR TODAS
           </button>
         </section>
-        <section className='grid-wrapper'>
-          {
-            apiMock.map((el, index) => (
-              <ToolCard
-                onClick={() => {
-                  setSelectedTool(el)
-                  handleToolSelect(el)
-                  setModalOpen(true)
-                }}
-                key={ index }
-                id={ el.app_id }
-                name={ el.name }
-                color={ el.color }
-                icon={ el.icon }
-                link={ el.link }
-              />
-            ))
-          }
-        </section>
-        <div className='pagination-wrapper'>
-          <p className='pagination-title'>
-            Página 1 de 4
-          </p>
-        </div>
+
+        {
+          loading ? (
+            <div className='loading-wrapper'>
+              <div>
+                <AiOutlineLoading size={'3.5em'} className='rotating' />
+              </div>
+            </div>
+          ) : (
+            <>
+              <section className='grid-wrapper'>
+                {
+                  api.map((el, index) => (
+                    <ToolCard
+                      onClick={() => {
+                        setSelectedTool(el)
+                        handleToolSelect(el)
+                        setModalOpen(true)
+                      }}
+                      key={ index }
+                      id={ el.app_id }
+                      name={ el.name }
+                      color={ el.color }
+                      icon={ el.icon }
+                      link={ el.link }
+                    />
+                  ))
+                }
+              </section>
+              <div className='pagination-wrapper'>
+                <p className='pagination-title'>
+                  Página 1 de 4
+                </p>
+              </div>            
+            </>
+
+          )
+        }
+
       </article>
       <Footer />
     </main>
