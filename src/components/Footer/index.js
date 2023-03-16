0254abd06aec748c401ae93dfcb36608851d6198
Leonardo <89220518+leodiman182@users.noreply.github.com>
2023-03-16 20:57:06 -0300

    Refact + mudança de link no footer para portfolio

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ const Footer = () => {
       <div className='footer-bar-div'>
         <img className='footer-logo' src={ plugaLogo } alt="" />
         <div className='footer-box'>
-          <p>{ year } | Desenvolvido por <a target='_blank' href="https://www.linkedin.com/in/leonardodiman/" rel="noopener noreferrer">Leonardo Diman</a></p>
+          <p>{ year } | Desenvolvido por <a target='_blank' href="https://portfolio-leodiman.vercel.app/" rel="noopener noreferrer">Leonardo Diman</a></p>
         </div>
       </div>
     </footer>
--- src/components/PaginatedItems/index.js
+++ src/components/PaginatedItems/index.js
@@ -12,29 +12,16 @@ const right = <AiFillCaretRight size={'2em'} className='left-arrow' />
 function PaginatedItems({ itemsPerPage }) {
   const { api } = useContext(MainContext);
 
-  // Here we use item offsets; we could also use page offsets
-  // following the API or data you're working with.
   const [itemOffset, setItemOffset] = useState(0);
 
-  // Simulate fetching items from another resources.
-  // (This could be items from props; or items loaded in a local state
-  // from an API endpoint with useEffect and useState)
   const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
   const currentItems = api.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(api.length / itemsPerPage);
 
-  // Invoke when user click to request another page.
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % api.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
     setItemOffset(newOffset);
   };
-
-  console.log(currentItems);
-
 
   return (
     <>
